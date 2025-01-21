9de6f136dc320ee2a33f371247df69a3bd01f569
Hoang Hai Anh <chanhmycute@gmail.com>
2025-01-21 14:11:04 +0700

    Format lại code

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -10,40 +10,37 @@ import java.awt.event.ActionListener;
 
 public class LoginController {
 
-    private final JPanel cardPanel;
-    private final CardLayout cardLayout;
     private final LoginPanel loginPanel;
     private final UserData userData;
+    private final JPanel cardPanel;
+    private final CardLayout cardLayout;
 
-    public LoginController(LoginPanel loginPanel , JPanel cardPanel,CardLayout cardLayout, UserData userData) {
-        this.cardLayout = cardLayout;
-        this.cardPanel = cardPanel;
+    public LoginController(LoginPanel loginPanel, JPanel cardPanel, CardLayout cardLayout, UserData userData) {
         this.loginPanel = loginPanel;
         this.userData = userData;
+        this.cardPanel = cardPanel;
+        this.cardLayout = cardLayout;
 
         loginPanel.getLoginButton().addActionListener(btnLogin());
     }
 
     private ActionListener btnLogin() {
-        return e -> {
-            handleLogin();
-        };
+        return e -> handleLogin();
     }
 
-    private void handleLogin(){
+    private void handleLogin() {
         loginPanel.clearErrorMessage();
         String username = loginPanel.getUsernameField().getText();
         String password = String.valueOf(loginPanel.getPasswordField().getPassword());
         if (validateCredentials(username, password)) {
-//            cardPanel.add(new GamePanel(), "Game");
-//            cardLayout.show(cardPanel, "Game");
-            loginPanel.displayErrorMessage("oke roi");
+            cardPanel.add(new GamePanel(), "Game");
+            cardLayout.show(cardPanel, "Game");
         } else {
             loginPanel.displayErrorMessage("Invalid username or password.");
         }
     }
 
-    private boolean validateCredentials(String username, String password){
+    private boolean validateCredentials(String username, String password) {
         return username.equals(this.userData.getUsername()) && password.equals(this.userData.getPassword());
     }
 }
